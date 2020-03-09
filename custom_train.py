fb16810ef30ef68224010edaab192506d86fb9f7
Yash <ygandhi100@gmail.com>
2020-03-09 12:33:51 -0600

    saving and loading models

--- custom_train.py
+++ custom_train.py
@@ -4,16 +4,17 @@ import gym
 import yaml
 import pickle
 
+from os.path import join
 from argparse import ArgumentParser
 from models.DQN import DQNAgent
 from models.networks import MlpPolicy, CNNPolicy
 from models.reward_functions import Identity, additive_SHAP
 
-
+from pdb import set_trace as debug
 
 def main(configs):
     
-
+    
     env = gym.make(configs['env'])
     if(configs['policy'].casefold() == 'Mlp'.casefold()):
         policy = MlpPolicy
@@ -28,14 +29,23 @@ def main(configs):
         reward = additive_SHAP
     
     model_building_configs = configs['model_building_parameters']
+    
+
     model = DQNAgent(env, 
                     policy, 
                     reward,
                     **model_building_configs)
 
 
+    if(configs['reload']):
+        assert configs['saved_model'] is not None, "if reload is true, then saved_model must be set"
+        path_to_saved_models = join('/'.join(model.save_path.split('/')[:-1]), configs['saved_model'])
+        model.load(path_to_saved_models)
+    
+    
     model.learn(configs['timesteps'])
-
+    with open(join(model.save_path, 'training_parameters.yaml'), 'w') as f:
+        yaml.dump(configs, f)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
