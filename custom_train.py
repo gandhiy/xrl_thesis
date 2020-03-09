b8ddea5f1fa7e4df56f98266518bf20778885d31
Yash <ygandhi100@gmail.com>
2020-03-08 23:49:19 -0600

    custom training

---
+++ custom_train.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import gym
+import pickle
+
+from models.DQN import DQNAgent
+from models.networks import MlpPolicy
+from models.reward_functions import Identity, additive_SHAP
+
+
+env = gym.make("CartPole-v0")
+model = DQNAgent(env, 
+                 MlpPolicy, 
+                 Identity, 
+                 model_name='test3',
+                 memory_size = 50000, 
+                 batch_size=256, 
+                 update_timesteps=256, 
+                 logger_steps=100, 
+                 learning_starts=0)
+
+
+model.learn(500)
+
+ 
+
