f431cdce81bdd47d23b06b42f0c1f483ce8db52f
Yash <ygandhi100@gmail.com>
2020-03-09 10:54:07 -0600

    moved to training with yaml file

--- custom_train.py
+++ custom_train.py
@@ -1,26 +1,52 @@
 import os
 import sys
 import gym
+import yaml
 import pickle
 
+from argparse import ArgumentParser
 from models.DQN import DQNAgent
-from models.networks import MlpPolicy
+from models.networks import MlpPolicy, CNNPolicy
 from models.reward_functions import Identity, additive_SHAP
 
 
-env = gym.make("CartPole-v0")
-model = DQNAgent(env, 
-                 MlpPolicy, 
-                 Identity, 
-                 model_name='test',
-                 memory_size = 50000, 
-                 batch_size=256, 
-                 update_timesteps=256, 
-                 logger_steps=100, 
-                 learning_starts=0)
+
+def main(configs):
+    
+
+    env = gym.make(configs['env'])
+    if(configs['policy'].casefold() == 'Mlp'.casefold()):
+        policy = MlpPolicy
+    elif(configs['policy'].casefold() == 'CNN'.casefold()):
+        policy = CNNPolicy
+    else:
+        raise AttributeError("need to specify policy either ['Mlp', 'CNN'] ")
+
+    if(configs['reward'].casefold() == 'Identity'.casefold()):
+        reward = Identity
+    elif(configs['reward'].casefold() == 'additive_SHAP'.casefold()):
+        reward = additive_SHAP
+    
+    model_building_configs = configs['model_building_parameters']
+    model = DQNAgent(env, 
+                    policy, 
+                    reward,
+                    **model_building_configs)
 
 
-model.learn(500)
+    model.learn(configs['timesteps'])
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--configs', required=True, help='configuration file')
+    args = parser.parse_args()
+
+    with open(args.configs, 'rb') as f:
+        configs = yaml.load(f)
+
+    main(configs)
+
 
  
 
